60ded63c0acf6242e328a8f3f9e036ca397864c2
manishapatil30 <manisha@mtouchlabs.com>
2021-03-26 12:42:59 +0530

    master

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TermsandconditionsComponent } from './termsandconditions/termsandconditions.component';
+import { HomeComponent } from './home/home.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home/terms_conditions', pathMatch: 'full' },
+  {
+    path: 'home', component: HomeComponent, children: [
+      { path: '', redirectTo: 'homepage', pathMatch: 'full' },
+      {
+        path: 'terms_conditions', component: TermsandconditionsComponent
+      }
+   ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TermsandconditionsComponent } from './termsandconditions/termsandconditions.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TermsandconditionsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
